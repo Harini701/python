cb5199fc033b31e13899dac2c7f28bc484120ec9
Harini701 <113083879+Harini701@users.noreply.github.com>
2022-10-03 22:58:54 +0530

    Create diamond star.py

---
+++ diamond star.py
@@ -0,0 +1,19 @@
+r=int(input("Enter the number of rows: "))
+k=2*r-2
+for i in range(0,r):
+    for j in range(k,0,-1):
+        print(end=" ")
+    k=k-1
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print("\r")
+
+k=r-2
+
+for i in range(r,-1,-1):
+    for j in range(k,0,-1):
+        print(end=" ")
+    k=k+1
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print("\r")
