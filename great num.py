ed5da2790fa60f78f20bd28943d7ec042ef50103
Harini701 <113083879+Harini701@users.noreply.github.com>
2022-10-03 22:52:45 +0530

    Create great num.py

---
+++ great num.py
@@ -0,0 +1,10 @@
+rint("Enter the values of three numbers: ")
+a=int(input())
+b=int(input())
+c=int(input())
+if(a>b)and(a>c):
+    print("a is greater")
+elif(b>a)and(b>c):
+    print("b is greater")
+else:
+    print("c is greater")
